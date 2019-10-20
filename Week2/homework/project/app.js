61847f91cb1f51f323f5fbac9fdab109b19bf502
Ali Dagdeviren <54191107+adagdeviren@users.noreply.github.com>
2019-10-20 02:40:41 +0200

    work in progress..(Trying to understand and build the logic of a countdown timer)

---
+++ Week2/homework/project/app.js
@@ -0,0 +1,18 @@
+/* the requirements:
+
+If the timer is running, the user can't change the session length anymore
+Use at least 3 functions
+Display minutes and seconds
+If the timer finishes the timer should be replaced by the message: Time's up! */
+
+// ===================WORK IN PROGRESS===================//
+
+{
+
+"use strict";
+
+
+
+
+
+}
